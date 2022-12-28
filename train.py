7cdd6a9d2fa1b014b07144330e766f830bf0ff14
Monis <liem@epicliem.com>
2022-12-28 15:23:44 -0500

    emphisize capturing pieces more

--- train.py
+++ train.py
@@ -1,52 +1,39 @@
-import numpy as np
-import pandas as pd
-import inspect
 import gc
+import logging
+import os
 
-import chess
-from chess.pgn import Game
-import RLC
-from RLC.real_chess.environment import Board
-from RLC.real_chess.learn import TD_search
 from RLC.real_chess.agent import Agent
 from RLC.real_chess.agent import RandomAgent
-
+from RLC.real_chess.environment import Board
+from RLC.real_chess.learn import TD_search
 from keras.models import load_model
 
-def my_reset(*varnames):
-    """
-    varnames are what you want to keep
-    """
-    globals_ = globals()
-    to_save = {v: globals_[v] for v in varnames}
-    to_save['my_reset'] = my_reset  # lets keep this function by default
-    del globals_
-    globals().update(to_save)
+logging.basicConfig(filename="training.log", level=logging.INFO)
 
-t = 20
+t = 50
 
-agent = Agent(network='convo',lr=0.07)
-ragent = RandomAgent()
-board = Board(ragent)
-R = TD_search(env=board , agent=agent)
-R.agent.fix_model()
-R.agent.model.summary()
+agent = Agent(network='big',lr=0.5)
 
+if not os.path.isfile('agent_model.h5'):
+    print("No model detected. Creating new model.")
+    logging.info("No model detected. Creating new model.")
+    ragent = RandomAgent()
+    board = Board(ragent, capture_reward_factor=1)
+    R = TD_search(env=board, agent=agent, memsize=6000)
+    finalboard = R.learn(iters=100)
+    logging.info("New model created that is trained to capture.")
+    R.agent.model.save('agent_model.h5')
+    logging.info("model saved as agent_model.h5")
 
-model = R.learn(iters=1, maxiter=30, c=1) # training agent against random moves with small maxiter
-
-
-# R.agent.model.save('agent_model.h5')
-print("first training step done")
-
-for _ in range(t):
+for i in range(t):
     gc.collect()
     board = Board(agent)
     agent.model = load_model('agent_model.h5')
-    R = TD_search(env=board, agent=agent)
-    R.learn(iters=25, maxiter=120-(t*2), c=5)
+    R = TD_search(env=board, agent=agent, memsize=6000)
+    finalboard = R.learn(iters=25, maxiter=120-(t*2), c=5)
+    print(finalboard)
     R.agent.model.save('agent_model.h5')
-
+    logging.info(f"{i} model variations done \n {finalboard} \n \n")
 
 
 
